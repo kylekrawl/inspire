a8e1662b645a31910c8fa12852c51e3ee2d372e7
Kyle Krawl <kyle.krawl@gmail.com>
2017-11-12 19:17:49 -0700

    Added test for valid image urls from image API.

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -2,13 +2,31 @@ function ImageController() {
 	var imageService = new ImageService()
 	//Your ImageService is a global constructor function what can you do here if you new it up?
 
+	function validImageUrl(url) {
+		var valid = false
+		var extensions = ['.jpg', 'jpeg', '.png']
+		for (var i in extensions) {
+			var extension = extensions[i]
+			if (url.toLowerCase().substring(url.length - extension.length) === extension) {
+				valid = true
+				break
+			}
+		}
+		return valid
+	}
+
 	//pass getImage draw function as callback: 
 	function drawBackgroundImage(image) {
 		console.log('Image data: ', image)
 		var elem = document.getElementById('body')
 		if (image.large_url) {
+			if (validImageUrl(image.large_url)) {
 			elem.style.cssText = `background: url("${image.large_url}") center no-repeat; background-size: cover;`
 			console.log('Large image url: ', image.large_url)
+			} else {
+				console.log('Image url failed validity test, fetching new image.')
+				imageService.getImage(drawBackgroundImage)
+			}
 		} else {
 			console.log('No large image url, fetching new image.')
 			imageService.getImage(drawBackgroundImage)
